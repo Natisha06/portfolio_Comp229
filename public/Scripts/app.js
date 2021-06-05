df307a1210e64b27b70602b624e71b4a1ba1fa98
Natisha2406 <64129176+Natisha2406@users.noreply.github.com>
2021-06-04 21:41:20 -0400

    portfolio: Views, Partials, Header, Nav,ABout me, Services,Projects, Contact us page added

---
+++ public/Scripts/app.js
@@ -0,0 +1,17 @@
+/* File Name: app.css
+Name: Natisha Singla
+Id: 301145778
+Date: 06/04/2021 */
+
+// IIFE -- immediately invoked functions
+
+// function to print console when app starts
+(function(){
+
+    function Start()
+    {
+        console.log("App Started..");
+    }
+
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index_home', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index_about', { title: 'About Me' });
+});
+
+/* GET Project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index_contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
