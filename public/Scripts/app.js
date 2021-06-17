9477ee70dd22d4ea035050f913121671949e653d
Natisha2406 <64129176+Natisha2406@users.noreply.github.com>
2021-06-17 19:49:36 -0400

    Authentication added

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,13 +4,25 @@ Id: 301145778
 Date: 06/04/2021 */
 
 // IIFE -- immediately invoked functions
-
-// function to print console when app starts
 (function(){
 
     function Start()
     {
         console.log("App Started..");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger')
+
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event) => {
+                if(!confirm("Are you sure?"))
+                {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+
+            });
+        }
     }
 
     window.addEventListener("load", Start);
